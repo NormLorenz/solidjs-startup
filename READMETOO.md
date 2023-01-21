071bfdd4d27d96b4b701a77a9800894885868f6e
norm lorenz <normlorenz@gmail.com>
2023-01-21 08:43:37 -0800

    Built out main page

--- READMETOO.md
+++ READMETOO.md
@@ -6,3 +6,7 @@
 > npm install 
 
 ```
+
+- add solidjs logo
+- hook in router
+- hook in navigation
--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,56 @@ import type { Component } from 'solid-js';
 const App: Component = () => {
   return (
     <>
-      hi
+      <header class="container">
+        <section>
+          <article>
+            <nav>
+              <ul>
+                <li><strong>Solidjs App</strong></li>
+              </ul>
+              <ul>
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#" role="button" class="outline">Primary</a></li>
+              </ul>
+            </nav>
+          </article>
+        </section>
+      </header>
+
+      <main class="container">
+        <section>
+          <article>
+            <hgroup>
+              <h2>Join Us</h2>
+              <h3>Litora torquent per conubia nostra</h3>
+            </hgroup>
+            <form>
+              <div class="grid">
+                <input type="text" name="firstname" placeholder="First name" aria-label="First name" required />
+                <input type="email" name="email" placeholder="Email address" aria-label="Email address" required />
+                <button type="submit">Subscribe</button>
+              </div>
+              <fieldset>
+                <label for="terms">
+                  <input type="checkbox" role="switch" id="terms" name="terms" />
+                  I agree to the <a href="#">Privacy Policy</a>
+                </label>
+              </fieldset>
+            </form>
+
+          </article>
+        </section>
+      </main>
+
+      <footer class="container">
+        <section>
+          <article>
+            <span>Copyright © 2022 Water Street Works</span>
+          </article>
+        </section>
+      </footer>
     </>
-    // <div class={styles.App}>
-    //   <header class={styles.header}>
-    //     <img src={logo} class={styles.logo} alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       class={styles.link}
-    //       href="https://github.com/solidjs/solid"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn Solid
-    //     </a>
-    //   </header>
-    // </div>
   );
 };
 
