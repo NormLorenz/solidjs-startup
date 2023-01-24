7cd6be9911338cb41a78b5415e7be56bf9010a76
NormLorenz <normlorenz@gmail.com>
2023-01-24 12:39:05 -0800

    Update Counter.tsx

--- src/pages/Counter.tsx
+++ src/pages/Counter.tsx
@@ -1,8 +1,31 @@
+import { createSignal } from "solid-js";
+
 const ContactUs = () => {
+
+  const [count, setCount] = createSignal(0);
+
+  const increment = () => setCount(count() + 1);
+  const decrement = () => setCount(count() - 1);
+  const clear = () => setCount(0);
 
   return (
     <article>
-      <h2>Simple Counter</h2>
+      <hgroup>
+        <h2>Simple Counter</h2>
+        <h3>Work with a counter</h3>
+      </hgroup>
+      <div class="grid">
+        <div>
+          <button class="outline counter-button" onClick={() => increment()}>Increment</button>
+          <button class="outline counter-button" onClick={() => decrement()}>Decrement</button>
+          <button class="outline counter-button" onClick={() => clear()}>Clear</button>
+        </div>
+        <div></div>
+        <div class="counter-number">{count()}</div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </article>
   )
 }
