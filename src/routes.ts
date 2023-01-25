2729c545e5d8b8a8c81a31af0d8eb3022917af1e
NormLorenz <normlorenz@gmail.com>
2023-01-25 13:39:16 -0800

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -20,37 +20,4 @@ export const routes: RouteDefinition[] = [
     path: '/about',
     component: lazy(() => import('./pages/About')),
   },
-  // {
-  //   path: '/charts',
-  //   component: lazy(() => import('./pages/Charts/Charts')),
-  // },
-  // {
-  //   path: '/shopping',
-  //   component: lazy(() => import('./pages/Shopping/Shopping')),
-  // },
-  // {
-  //   path: '/about',
-  //   component: lazy(() => import('./pages/About')),
-  //   data: AboutData,
-  // },
-  // {
-  //   path: '/bookshelf',
-  //   component: lazy(() => import('./pages/BookShelf/BookShelf')),
-  // },
-  // {
-  //   path: '/tasks',
-  //   component: lazy(() => import('./pages/Tasks/Tasks')),
-  // },
-  // {
-  //   path: '/contactus',
-  //   component: lazy(() => import('./pages/ContactUs/ContactUs')),
-  // },
-  // // {
-  // //   path: ['login', 'sign-up'],
-  // //   component: lazy(() => import('./pages/Auth')),
-  // // },
-  // {
-  //   path: '**',
-  //   component: lazy(() => import('./pages/Error')),
-  // },
 ];
