2207313b9ed51e6013e2d8eed1407824bb2ccba9
NormLorenz <normlorenz@gmail.com>
2023-01-25 08:17:18 -0800

    Update Header.tsx

--- src/fragments/Header.tsx
+++ src/fragments/Header.tsx
@@ -31,6 +31,7 @@ const Header = () => {
           <li><a href="#/">Home</a></li>
           <li><a href="#/counter">Simple Counter</a></li>
           <li><a href="#/contactus">Contact Us</a></li>
+          <li><a href="#/about">About</a></li>
           <li>
             <button class="outline theme-button" onClick={() => { toggleTheme() }} type="submit">{toggleLabel()}</button>
           </li>
