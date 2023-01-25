4e0594f524e76247643ff32c64d446d8f2f25073
NormLorenz <normlorenz@gmail.com>
2023-01-25 13:16:07 -0800

    Create alertState.ts

---
+++ src/fragments/alertState.ts
@@ -0,0 +1,8 @@
+import { createSignal, createRoot } from "solid-js";
+
+const alertState = () => {
+  const [alert, setAlert] = createSignal(false);
+  return { alert, setAlert };
+}
+
+export default createRoot(alertState);
