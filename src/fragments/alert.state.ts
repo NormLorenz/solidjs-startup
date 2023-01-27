f1a0556c90030b9aedf24328fb49aa05bf14f911
NormLorenz <normlorenz@gmail.com>
2023-01-27 10:12:39 -0800

    Delete alert.state.ts

--- src/fragments/alert.state.ts
+++
@@ -1,8 +0,0 @@
-import { createSignal, createRoot } from 'solid-js';
-
-const alertState = () => {
-  const [alert, setAlert] = createSignal(false);
-  return { alert, setAlert };
-}
-
-export default createRoot(alertState);
