77309b90178981107bc55247027f1d6b192acd4c
NormLorenz <normlorenz@gmail.com>
2023-01-27 10:08:19 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
-      // VITE_API: JSON.stringify('https://jsonplaceholder.typicode.com/posts'),
-      // TE_API_URI: (env.VITE_API_URI)
-      // API: JSON.stringify('https://jsonplaceholder.typicode.com/posts'),
-      // APP_ENV: JSON.stringify(env.API_URI)
     },
     plugins: [solidPlugin()],
     server: {
