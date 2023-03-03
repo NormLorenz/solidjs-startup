5fe729f1676321a35549562f3fc9ef2984bde01b
NormLorenz <normlorenz@gmail.com>
2023-03-03 13:54:21 -0800

    Update BookList.tsx

--- src/fragments/BookList.tsx
+++ src/fragments/BookList.tsx
@@ -1,9 +1,10 @@
-import { For } from 'solid-js';
+import { For, Setter } from 'solid-js';
 
 import { Book } from '../models/bookShelf.model';
 
 interface BookListProps {
   books: Book[];
+  setBooks: Setter<Book[]>;
 }
 
 export const BookList = (props: BookListProps) => {
@@ -15,7 +16,9 @@ export const BookList = (props: BookListProps) => {
           {(book) => {
             return (
               <li>
-                {book.title} <span style={{ "font-style": "italic" }}> ({book.author})</span>
+                {book.title} <span style={{ "font-style": "italic" }}> ({book.author})</span>{" "}
+                {/* <img src="/src/assets/minus.svg" onclick={() => { props.setbooks((books) => [...books, book]); }}  alt="Delete" width="24" height="24" /> */}
+                <img src="/src/assets/minus.svg" alt="Delete" width="24" height="24" />
               </li>
             );
           }}
