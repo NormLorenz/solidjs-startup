12d1079e758de0f1df218ad580b84c6d5e387e2e
NormLorenz <normlorenz@gmail.com>
2023-03-03 13:53:18 -0800

    Update AddBook.tsx

--- src/fragments/AddBook.tsx
+++ src/fragments/AddBook.tsx
@@ -3,7 +3,7 @@ import { createSignal, Setter, createResource, For, Show } from "solid-js";
 import { Book } from "../models/Bookshelf.model";
 import { bookShelfService } from '../services/bookShelf.service';
 
-export interface AddBookProps {
+interface AddBookProps {
   setBooks: Setter<Book[]>;
 }
 
@@ -12,9 +12,7 @@ export const AddBook = (props: AddBookProps) => {
   const [input, setInput] = createSignal('');
   const [query, setQuery] = createSignal('');
 
-  // add a minus to listbooks to remove an existing book
-
-  const [data] = createResource<Book[], string>(query, bookShelfService);
+  const [books] = createResource<Book[], string>(query, bookShelfService);
 
   return (
     <>
@@ -23,13 +21,13 @@ export const AddBook = (props: AddBookProps) => {
         <button class="outline" type="submit" onClick={(e) => { e.preventDefault(); setQuery(input()); }}>Search</button>
       </form>
 
-      <Show when={!data.loading} fallback={<>Searching...</>}>
+      <Show when={!books.loading} fallback={<>Searching...</>}>
         <ul>
-          <For each={data()}>
+          <For each={books()}>
             {(book) => (
               <li>
                 {book.title} by {book.author}{" "}
-                <img src="/src/assets/plus.svg" onclick={(e) => { props.setBooks((books) => [...books, book]); }} alt="Add" width="24" height="24" />
+                <img src="/src/assets/plus.svg" onclick={() => { props.setBooks((books) => [...books, book]); }} alt="Add" width="24" height="24" />
               </li>
             )}
           </For>
